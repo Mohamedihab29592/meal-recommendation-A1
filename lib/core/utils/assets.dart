76e3a1f8e7ba93d404448f9fbb9117301ad7ea67
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-19 23:08:56 +0300

    Added new assets & Updated Assets, AppStrings  - Added onboarding images

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for fontsInterInter28ptBold
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOnboarding2
+  /// assets/images/onboarding2.png
+  static const String imagesOnboarding2 = "assets/images/onboarding2.png";
+
+  /// Assets for svgsAuthLogo
+  /// assets/svgs/auth-logo.svg
+  static const String svgsAuthLogo = "assets/svgs/auth-logo.svg";
+
+  /// Assets for svgsOnboardingLogo
+  /// assets/svgs/onboarding-logo.svg
+  static const String svgsOnboardingLogo = "assets/svgs/onboarding-logo.svg";
+}
