18e26e14455fbf553020c6aaedd573c964b36950
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-10-20 00:02:04 +0300

    Built FirebaseErrorHandler & Updated AppStrings  - Installed firebase_auth package  - Added noInternetConnection String to AppStrings

--- lib/core/utils/strings.dart
+++ lib/core/utils/strings.dart
@@ -4,4 +4,6 @@ class AppStrings {
   static const String fontFamily = 'Inter';
   static const String appTitle = 'Meal-Recommendation';
   static const String pleaseWait = 'Please wait';
+  static const String noInternetConnection =
+      'No internet connection. Please check your internet connection and try again.';
 }
