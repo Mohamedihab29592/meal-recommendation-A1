18186db6256dd50c9285f2ffcbc877d0e6655ab0
SalmaMarey <salmamarey2001@gmail.com>
2024-10-21 01:40:12 +0300

    edit duration

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,14 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );  await Future.delayed(const Duration(seconds: 10));
+  );
+  await Future.delayed(const Duration(seconds: 3));
 
   runApp(const MyApp());
-   FlutterNativeSplash.remove();
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
