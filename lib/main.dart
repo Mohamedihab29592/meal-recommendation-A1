7a2eb213344736e5fae74e271dfab7ab2bb6308e
SalmaMarey <salmamarey2001@gmail.com>
2024-10-20 19:02:42 +0300

    make splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:meal_recommendations/core/themes/app_themes.dart';
 import 'package:meal_recommendations/core/utils/strings.dart';
 import 'package:meal_recommendations/features/splash_boarding/splash_screen.dart';
@@ -7,10 +8,13 @@ import 'package:meal_recommendations/features/splash_boarding/splash_screen.dart
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
+   FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
