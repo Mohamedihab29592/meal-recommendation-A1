d306992940ca8f4cd34f18d6965ac422a5f4e346
Salma Marey <salmamarey2001@gmail.com>
2024-11-06 02:06:01 +0200

    install device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -14,30 +14,28 @@ import 'package:meal_recommendations/core/utils/functions/check_if_user_is_logge
 import 'package:meal_recommendations/core/utils/strings.dart';
 import 'core/models/meal.dart';
 import 'firebase_options.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
   Bloc.observer = MyBlocObserver();
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
- await Hive.initFlutter();
- await Hive.openBox('appBox'); 
- 
-  
-
-  Hive.openBox('myFavMeals');
+  await Hive.initFlutter();
+  await Hive.openBox('appBox');
+  await Hive.openBox('myFavMeals');
   Hive.registerAdapter(MealAdapter());
   Hive.registerAdapter(MealSummaryAdapter());
   Hive.registerAdapter(MealNutritionAdapter());
   Hive.registerAdapter(MealIngredientAdapter());
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await checkIfUserIsLoggedIn();
-
 
   runApp(const MyApp());
   FlutterNativeSplash.remove();
@@ -48,20 +46,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    return ScreenUtilInit(
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (_, child) {
-          return MaterialApp(
-            onGenerateRoute: AppRouter.onGenerateRoute,
-            title: AppStrings.appTitle,
-            debugShowCheckedModeBanner: false,
-            theme: AppThemes.lightTheme,
-
-            initialRoute: Routes.splash,
-
-          );
-        });
+  
+    return DevicePreview(
+     enabled: true,
+      builder: (context) {
+        return ScreenUtilInit(
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (_, child) {
+            return MaterialApp(
+              onGenerateRoute: AppRouter.onGenerateRoute,
+              title: AppStrings.appTitle,
+              debugShowCheckedModeBanner: false,
+              theme: AppThemes.lightTheme,
+              initialRoute: Routes.splash,
+            );
+          }
+        );
+      },
+    );
   }
 }
