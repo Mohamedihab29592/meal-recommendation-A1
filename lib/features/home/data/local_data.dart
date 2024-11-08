41a41f57f2e8711da59b88fece484d64113bb4ce
Mahmoud Youssef <mahmoudyousse220@gmail.com>
2024-11-08 13:40:44 +0200

    edit hive

--- lib/features/home/data/local_data.dart
+++ lib/features/home/data/local_data.dart
@@ -4,16 +4,19 @@ import 'package:meal_recommendations/core/models/meal.dart';
 class LocalData {
   var box = Hive.box('myFavMeals');
   void addMealToFav(Meal meal) {
-    box.add(meal);
+    box.put(meal.name, meal);
     print('Meal added to favorites');
     print(box.length);
   }
 
-  void removeFavMeal(Meal meal) {
-    box.delete(meal);
-
-    print('Meal deleted successfully');
-    print(box.length);
+  Future<void> removeFavMeal(Meal meal) async {
+    if (box.containsKey(meal.name)) {
+      await box.delete(meal.name);
+      print('${meal.name} deleted successfully');
+    } else {
+      print('${meal.name} not found in favorites');
+    }
+    print('Total favorite meals: ${box.length}');
   }
 
   void removeAllMeals() {
